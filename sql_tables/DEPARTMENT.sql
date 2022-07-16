c3322387436ca5ba0b37fbb083e6ccf4ff8e9bd6
Dipta <sheikhdipta@gmail.com>
2022-07-16 21:32:27 +0600

    added all the sql tables

---
+++ sql_tables/DEPARTMENT.sql
@@ -0,0 +1,5 @@
+INSERT INTO "DEPARTMENT"("DID", "NAME") VALUES ('1', 'CSE                                               ');
+INSERT INTO "DEPARTMENT"("DID", "NAME") VALUES ('2', 'EEE                                               ');
+INSERT INTO "DEPARTMENT"("DID", "NAME") VALUES ('3', 'ME                                                ');
+INSERT INTO "DEPARTMENT"("DID", "NAME") VALUES ('4', 'CE                                                ');
+INSERT INTO "DEPARTMENT"("DID", "NAME") VALUES ('5', 'MME                                               ');
